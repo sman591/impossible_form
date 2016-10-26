bac938109ce8ed06f564b6791f6ca34ff5cdd087
Stuart Olivera <stuart@stuartolivera.com>
2016-10-25 23:53:38 -0700

    Require email for submit endpoint

--- app/controllers.rb
+++ app/controllers.rb
@@ -1,14 +1,17 @@
 ImpossibleForm::App.controllers  do
 
   homepage = 'https://piedoom.github.io/impossible/'
+  failed_homepage = "#{homepage}?email=failed"
 
   get '/' do
     redirect homepage
   end
 
   post '/submit' do
+    redirect(failed_homepage) unless params['email']
+
     subscriber = Subscriber.new(email: params['email'])
-    destination = subscriber.save ? homepage : "#{homepage}?email=failed"
+    destination = subscriber.save ? homepage : failed_homepage
     redirect(destination)
   end
 
